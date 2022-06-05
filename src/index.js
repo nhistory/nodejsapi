5174950c06762606fd7b26ff34420122053758d0
nhistory <nhistory@naver.com>
2022-06-05 15:51:59 -0300

    Modify index.js for using response.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import ip from 'ip';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import Response from './domain/response.js';
 
 dotenv.config();
 const PORT = process.env.SERVER_PORT || 3000;
@@ -9,10 +10,10 @@ const app = express();
 app.use(cors({ origin: '*' }));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send({ message: 'UP' });
-});
+app.get('/', (req, res) =>
+    res.send(new Response(200, 'OK', 'Patient API, v1.0.0 - All Systems Go'))
+);
 console.log(process.env);
 app.listen(PORT, () =>
-    console.log(`Server runing on: ${ip.address()}:${PORT}`)
+    console.log(`Server running on: ${ip.address()}:${PORT}`)
 );
