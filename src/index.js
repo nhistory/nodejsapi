9b28283848b852a7a403d8c5fb4bfe444495737a
nhistory <nhistory@naver.com>
2022-06-10 18:52:57 -0300

    Develope router module and connect with index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import cors from 'cors';
 import Response from './domain/response.js';
 import logger from './util/logger.js';
 import httpStatus from './controller/patient.controller.js';
+import patientRoutes from './route/patient.route';
 
 dotenv.config();
 const PORT = process.env.SERVER_PORT || 3000;
@@ -12,6 +13,7 @@ const app = express();
 app.use(cors({ origin: '*' }));
 app.use(express.json());
 
+app.use('/patients', patientRoutes);
 app.get('/', (req, res) =>
     res.send(
         new Response(
@@ -20,6 +22,17 @@ app.get('/', (req, res) =>
             'Patient API, v1.0.0 - All Systems Go'
         )
     )
+);
+app.all('*', (req, res) =>
+    res
+        .status(httpStatus.NOT_FOUND.code)
+        .send(
+            new Response(
+                httpStatus.NOT_FOUND.code,
+                httpStatus.NOT_FOUND.status,
+                'Route does not exist on the server'
+            )
+        )
 );
 app.listen(PORT, () =>
     logger.info(`Server running on: ${ip.address()}:${PORT}`)
---
+++ src/route/patient.route.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import {
+    getPatients,
+    createPatient,
+    getPatient,
+    deletePatient,
+    updatePatient,
+} from '../controller/patient.controller';
+
+const patientRoutes = express.Router();
+
+patientRoutes.route('/').get(getPatients).post(createPatient);
+patientRoutes
+    .route('/:id')
+    .get(getPatient)
+    .post(updatePatient)
+    .delete(deletePatient);
+
+export default patientRoutes;
